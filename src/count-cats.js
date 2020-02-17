2b0a8bc84e71886863cf3f2a5ddb1f2d7be11ad9
Anton Svirski <svirski89@gmail.com>
2020-02-17 19:34:34 +0300

    start coun cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,16 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(matrix) {
+ // throw 'Not implemented';
   // remove line with error and write your code here
+  let count = 0;
+
+  for (i = 0; i < matrix.length; i++){
+    for (j = 0; j < matrix[i].length; j++){
+      if(matrix[i][j] == '^^'){
+        count += 1;
+      }
+
+    }
+
+  }
+  return count;
 };
