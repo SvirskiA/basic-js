6d8d95226a7a7a15e1823d9d45c54a68ef7f0cac
Anton Svirski <svirski89@gmail.com>
2020-02-27 23:48:36 +0300

    init: transform -array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(array) {
+	if(array){
+		for (let i = 0; i < array.length; i++){
+            if (array[i]){
+                if (array[i] == '--discard-next'){
+                    array.splice(i,2);
+                } 
+                if (array[i] == '--discard-prev' && i > 0){
+                    array.splice(i-1,2);
+                } else if (array[i] == '--discard-prev'){
+                    array.splice(i,1);
+                }
+                if (array[i] == '--double-next'){
+                    array.splice(i,1);
+                    array.splice(i,0,array[i]);
+                }
+                if (array[i] == '--double-prev' && i > 0){
+                    array.splice(i,1);
+                    array.splice(i,0,array[i-1]);
+                } else if(array[i] == '--double-prev'){
+                    array.splice(i,1);
+                }
+            }
+			
+		}	
+	}
+	return array;
 };
