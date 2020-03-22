59336de19b717ad3ace7ef1c9ebb0b02fdaf49d7
Anton Svirski <svirski89@gmail.com>
2020-03-22 21:34:55 +0300

    add hanoi

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,20 +1,20 @@
 module.exports = class DepthCalculator {
     calculateDepth(arr) {
         let deep = 0;
-        function howDeep(arr){
-            for (let i = 0; i < arr.length; i++){
-                if(arr[i].length >= 0 && typeof(arr[0] != 'string')){
+        function howDeep(arr) {
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i].length >= 0 && typeof (arr[0] != 'string')) {
                     deep++;
-                    howDeep(arr[i])                   
+                    howDeep(arr[i])
                 }
             }
         }
-        for (let i = 0; i < arr.length; i++){
-            if(arr[i].length >= 0 && typeof(arr[0] != 'string')){
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].length >= 0 && typeof (arr[0] != 'string')) {
                 deep++;
                 howDeep(arr[i])
             }
         }
-return ++deep;
+        return ++deep;
     }
 };
