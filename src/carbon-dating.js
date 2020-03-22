d467ba20c609131c67efddc1f716f27d4df84eb1
Anton Svirski <svirski89@gmail.com>
2020-03-22 20:05:21 +0300

    fix carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,10 +2,12 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (arguments[0] && typeof(sampleActivity) === 'string' && sampleActivity <= 15 && sampleActivity > 0){
-    return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity)/ ( 0.693 / HALF_LIFE_PERIOD));
+  if (arguments[0] && 
+      typeof(sampleActivity) === 'string' && 
+      parseFloat(sampleActivity) <= 15 && 
+      parseFloat(sampleActivity) > 0){
+    return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))/ ( 0.693 / HALF_LIFE_PERIOD));
   } else {
     return false;
   }
-  
 };
