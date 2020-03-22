cae922d007cb82692215857d1498a12e25fe53db
Anton Svirski <svirski89@gmail.com>
2020-03-22 21:14:24 +0300

    add: weather

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,26 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+  if (!date) return 'Unable to determine the time of year!';
+
+  let m = date.getMonth() + 1;
+
+  if (m < 3 || m > 11) return 'winter';
+
+  else if (m > 2 && m < 6) return 'spring';
+
+  else if (m > 5 && m < 9) return 'summer';
+
+  else if (m > 8 && m < 12) return 'autumn';
+
+
+  // } else if(arr[1] > 1 && arr[1] < 5){
+  //   return 'spring';
+  // } else if(arr[1] > 4 && arr[1] < 8){
+  //   return 'summer';
+  // } else if(arr[1] > 7 && arr[1] < 10){
+  //   return 'autumn';
+  // }
 };
+
+
+//throw 'Not implemented';
